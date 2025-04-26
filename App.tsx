174e97884cdf69e823170ef17140ed4d6a255608
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-25 22:44:02 -0300

    Commit Inicial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,66 @@
+// Importa modulos necessários do React, React Native e Componentes
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// Define o componente do App
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* Quadrado vermelho no canto superior esquerdo */}
+      <View style={styles.quadradoVermelho}/>
+      
+      {/* Círculo azul no meio da tela */}
+      <View style={styles.circuloAzul}/>
+
+      {/* View com texto cinza no canto inferior direto */}
+      <View style={styles.textoContainer}>
+          <Text style={styles.texto}>Olá, React Native!</Text>  
+      </View>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  quadradoVermelho: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: 200,
+    height: 200,
+    backgroundColor: 'red',
+  },
+  circuloAzul: {
+    position: 'absolute',
+    width: 200,
+    height: 200,
+    backgroundColor: 'blue',
+    borderRadius: 100,
+  },
+  textoContainer: {
+    marginTop: 400,
+    padding: 40,
+    backgroundColor: 'lightgray',
+    borderRadius: 5,
+    top: 50,
+    left: 50,
+    width: 350,
+    height: 200,
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  texto: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+})
+
+// Exporta o componente App como padrão
+export default App;
